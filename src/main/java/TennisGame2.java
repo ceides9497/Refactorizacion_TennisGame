7a15cb3c1ecd2b4c9a5965c17e388aa599b3463f
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-02-24 01:20:00 -0400

    Numeros magicos

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,7 +1,12 @@
 
 public class TennisGame2 implements TennisGame
 {
-    public int player1Points = 0;
+    private static final int _FORTY = 3;
+	private static final int _THIRTY = 2;
+	private static final int _FIFTEEN = 1;
+	private static final int _LOVE = 0;
+	private static final int _MATCHPOINT = 4;
+	public int player1Points = 0;
     public int player2Points = 0;
     
     public String convertToLiteralPlayer1Points = "";
@@ -22,11 +27,11 @@ public class TennisGame2 implements TennisGame
     }
 
 	private String winingPlayerIsNotDeuce(String score, int player1Points2, int player2Points2) {
-		if (player1Points2>player2Points2 && player1Points2 < 4)
+		if (player1Points2>player2Points2 && player1Points2 < _MATCHPOINT)
         {
             score = thirty_forty_or_fifteen_for_player1(player1Points2, player2Points2);
         }
-        if (player2Points2>player1Points2 && player2Points2 < 4)
+        if (player2Points2>player1Points2 && player2Points2 < _MATCHPOINT)
         {
             score = thirty_forty_or_fifteen_for_player2(player1Points2,	player2Points2);
         }
@@ -35,13 +40,13 @@ public class TennisGame2 implements TennisGame
 
 	private String thirty_forty_or_fifteen_for_player2(int player1Points2, int player2Points2) {
 		String score;
-		if (player2Points2==2)
+		if (player2Points2==_THIRTY)
 		    convertToLiteralPlayer2Points="Thirty";
-		if (player2Points2==3)
+		if (player2Points2==_FORTY)
 		    convertToLiteralPlayer2Points="Forty";
-		if (player1Points2==1)
+		if (player1Points2==_FIFTEEN)
 		    convertToLiteralPlayer1Points="Fifteen";
-		if (player1Points2==2)
+		if (player1Points2==_THIRTY)
 		    convertToLiteralPlayer1Points="Thirty";
 		score = convertToLiteralPlayer1Points + "-" + convertToLiteralPlayer2Points;
 		return score;
@@ -49,38 +54,38 @@ public class TennisGame2 implements TennisGame
 
 	private String thirty_forty_or_fifteen_for_player1(int player1Points2, int player2Points2) {
 		String score;
-		if (player1Points2==2)
+		if (player1Points2==_THIRTY)
 		    convertToLiteralPlayer1Points="Thirty";
-		if (player1Points2==3)
+		if (player1Points2==_FORTY)
 		    convertToLiteralPlayer1Points="Forty";
-		if (player2Points2==1)
+		if (player2Points2==_FIFTEEN)
 		    convertToLiteralPlayer2Points="Fifteen";
-		if (player2Points2==2)
+		if (player2Points2==_THIRTY)
 		    convertToLiteralPlayer2Points="Thirty";
 		score = convertToLiteralPlayer1Points + "-" + convertToLiteralPlayer2Points;
 		return score;
 	}
 
 	private String winningPlayer(String score, int player1Points2, int player2Points2) {
-		if (player1Points2 > 0 && player2Points2==0)
+		if (player1Points2 > _LOVE && player2Points2==_LOVE)
         {
-            if (player1Points2==1)
+            if (player1Points2==_FIFTEEN)
                 convertToLiteralPlayer1Points = "Fifteen";
-            if (player1Points2==2)
+            if (player1Points2==_THIRTY)
                 convertToLiteralPlayer1Points = "Thirty";
-            if (player1Points2==3)
+            if (player1Points2==_FORTY)
                 convertToLiteralPlayer1Points = "Forty";
             
             convertToLiteralPlayer2Points = "Love";
             score = convertToLiteralPlayer1Points + "-" + convertToLiteralPlayer2Points;
         }
-        if (player2Points2 > 0 && player1Points2==0)
+        if (player2Points2 > _LOVE && player1Points2==_LOVE)
         {
-            if (player2Points2==1)
+            if (player2Points2==_FIFTEEN)
                 convertToLiteralPlayer2Points = "Fifteen";
-            if (player2Points2==2)
+            if (player2Points2==_THIRTY)
                 convertToLiteralPlayer2Points = "Thirty";
-            if (player2Points2==3)
+            if (player2Points2==_FORTY)
                 convertToLiteralPlayer2Points = "Forty";
             
             convertToLiteralPlayer1Points = "Love";
@@ -90,27 +95,27 @@ public class TennisGame2 implements TennisGame
 	}
 
 	private String tiedAndNotDeuce(String score, int player1Points2, int player2Points2) {
-		if (player1Points2 == player2Points2 && player1Points2 < 4)
+		if (player1Points2 == player2Points2 && player1Points2 < _MATCHPOINT)
         {
-            if (player1Points2==0)
+            if (player1Points2==_LOVE)
                 score = "Love";
-            if (player1Points2==1)
+            if (player1Points2==_FIFTEEN)
                 score = "Fifteen";
-            if (player1Points2==2)
+            if (player1Points2==_THIRTY)
                 score = "Thirty";
             score += "-All";
         }
-        if (player1Points2==player2Points2 && player1Points2>=3)
+        if (player1Points2==player2Points2 && player1Points2>=_FORTY)
             score = "Deuce";
 		return score;
 	}
 
 	private String winForPlayer1OrPlayer2(String score, int player1Points2, int player2Points2) {
-		if (player1Points2>=4 && player2Points2>=0 && (player1Points2-player2Points2)>=2)
+		if (player1Points2>=_MATCHPOINT && player2Points2>=0 && (player1Points2-player2Points2)>=_THIRTY)
         {
             score = "Win for player1";
         }
-        if (player2Points2>=4 && player1Points2>=0 && (player2Points2-player1Points2)>=2)
+        if (player2Points2>=_MATCHPOINT && player1Points2>=0 && (player2Points2-player1Points2)>=_THIRTY)
         {
             score = "Win for player2";
         }
@@ -118,12 +123,12 @@ public class TennisGame2 implements TennisGame
 	}
 
 	private String playerAdvantage(String score, int player1Points2, int player2Points2) {
-		if (player1Points2 > player2Points2 && player2Points2 >= 3)
+		if (player1Points2 > player2Points2 && player2Points2 >= _FORTY)
         {
             score = "Advantage player1";
         }
         
-        if (player2Points2 > player1Points2 && player1Points2 >= 3)
+        if (player2Points2 > player1Points2 && player1Points2 >= _FORTY)
         {
             score = "Advantage player2";
         }
