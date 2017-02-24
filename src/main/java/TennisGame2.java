3ad52714b957514f501719e08db9d46eb8ef1b32
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-02-24 00:23:56 -0400

    Borrando variables sin uso

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -6,13 +6,7 @@ public class TennisGame2 implements TennisGame
     
     public String convertToLiteralPlayer1Points = "";
     public String convertToLiteralPlayer2Points = "";
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+    public TennisGame2(String player1Name, String player2Name) {}
 
     public String getScore(){
         String score = "";
